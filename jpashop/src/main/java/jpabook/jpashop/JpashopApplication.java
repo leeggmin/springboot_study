5adce56689a8e33ecb31d43995c4587fa00fd15f
이지민 <wlals060330@gmail.com>
2022-05-31 22:15:38 +0900

    preferences

---
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+
+		Hello hello=new Hello();
+		hello.setData("f");
+		String data = hello.getData();
+		System.out.println("data : "+data);
+
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
