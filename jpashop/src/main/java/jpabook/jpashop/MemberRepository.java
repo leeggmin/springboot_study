9221d8f51085903c1a6b3c4ab6b90fc713811c30
이지민 <wlals060330@gmail.com>
2022-06-07 20:48:39 +0900

    entity

--- jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -11,12 +11,12 @@ public class MemberRepository {
     @PersistenceContext
     private EntityManager em;
 
-    public Long save(Member member){
-        em.persist(member);
-        return member.getId();
-    }
-
-    public Member find(long id){
-        return em.find(Member.class, id);
-    }
+//    public Long save(Member member){
+//        em.persist(member);
+//        return member.getId();
+//    }
+//
+//    public Member find(long id){
+//        return em.find(Member.class, id);
+//    }
 }
