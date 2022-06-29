0204e4e8eda23c5fde5a6aed71837ec2d290d546
이지민 <wlals060330@gmail.com>
2022-06-30 08:26:46 +0900

    controller

--- jpashop/src/main/java/jpabook/jpashop/controller/HomeController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -1,15 +1,17 @@
 package jpabook.jpashop.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController {
+@Slf4j
+public class HomeController {
 
-    @GetMapping("/hello")
-    public String hello(Model model){
-        model.addAttribute("data","hello!");
-        return "hello";
+    @GetMapping("/")
+    public String home(){
+       log.info("home controller");
+        return "home";
     }
 }
