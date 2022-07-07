8ee20082cb552af31ddaa9c867777d99ce9c1ce1
이지민 <wlals060330@gmail.com>
2022-07-07 22:17:07 +0900

    controller

--- jpashop/src/main/java/jpabook/jpashop/controller/HomeController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -2,7 +2,6 @@ package jpabook.jpashop.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
