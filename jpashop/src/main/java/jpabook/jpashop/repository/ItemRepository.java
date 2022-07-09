149e01872be669431cbb5dc2d1545bf8a90218ac
이지민 <wlals060330@gmail.com>
2022-07-09 22:09:25 +0900

    repository

--- jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -3,7 +3,6 @@ package jpabook.jpashop.repository;
 import jpabook.jpashop.domain.item.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.util.List;
