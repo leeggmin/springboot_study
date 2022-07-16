9336b582ba487ce3c286a492698daebbbe609050
이지민 <wlals060330@gmail.com>
2022-07-16 23:58:12 +0900

    initDB

---
+++ jpashop/src/main/java/jpabook/jpashop/InitDb.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+
+//총 주문 2개
+//    userA
+//        JPA1 BOOK
+//        JPA2 BOOK
+//    userB
+//        SPRING1 BOOK
+//        SPRING2 BOOK
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+
+    private final InitService initService;
+
+    @PostConstruct
+    public void init(){
+        initService.dbInit1();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService{
+        private EntityManager em;
+        public void dbInit1(){
+            Member member = new Member();
+            member.setName("userA");
+            member.setAddress(new Address("서울","1","1111"));
+            em.persist(member);
+        }
+    }
+}
