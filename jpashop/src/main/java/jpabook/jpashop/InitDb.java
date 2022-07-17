b0fdbeb21a5c7e048eb06691f1a1c82ba9825ddc
이지민 <wlals060330@gmail.com>
2022-07-17 23:29:31 +0900

    initDB

--- jpashop/src/main/java/jpabook/jpashop/InitDb.java
+++ jpashop/src/main/java/jpabook/jpashop/InitDb.java
@@ -1,7 +1,7 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.domain.Address;
-import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.*;
+import jpabook.jpashop.domain.item.Book;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +16,7 @@ import javax.persistence.EntityManager;
 //    userB
 //        SPRING1 BOOK
 //        SPRING2 BOOK
+//
 @Component
 @RequiredArgsConstructor
 public class InitDb {
@@ -37,6 +38,26 @@ public class InitDb {
             member.setName("userA");
             member.setAddress(new Address("서울","1","1111"));
             em.persist(member);
+
+            Book book1 = new Book();
+            book1.setName("JPA1 BOOK");
+            book1.setPrice(10000);
+            book1.setStockQuantity(100);
+            em.persist(book1);
+
+            Book book2 = new Book();
+            book2.setName("JPA2 BOOK");
+            book2.setPrice(20000);
+            book2.setStockQuantity(100);
+            em.persist(book2);
+
+            OrderItem orderItem1 = OrderItem.creteOrderItem(book1, 10000, 1);
+            OrderItem orderItem2 = OrderItem.creteOrderItem(book2, 20000, 2);
+
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress());
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
         }
     }
 }
