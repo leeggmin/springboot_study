ecd9dfcedca49960d8eb5c9f61b9eceb67460667
이지민 <wlals060330@gmail.com>
2022-07-18 14:37:20 +0900

    initDB

--- jpashop/src/main/java/jpabook/jpashop/InitDb.java
+++ jpashop/src/main/java/jpabook/jpashop/InitDb.java
@@ -26,13 +26,14 @@ public class InitDb {
     @PostConstruct
     public void init(){
         initService.dbInit1();
+        initService.dbInit2();
     }
 
     @Component
     @Transactional
     @RequiredArgsConstructor
     static class InitService{
-        private EntityManager em;
+        private final EntityManager em;
         public void dbInit1(){
             Member member = new Member();
             member.setName("userA");
@@ -53,6 +54,33 @@ public class InitDb {
 
             OrderItem orderItem1 = OrderItem.creteOrderItem(book1, 10000, 1);
             OrderItem orderItem2 = OrderItem.creteOrderItem(book2, 20000, 2);
+
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress());
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+        }
+
+        public void dbInit2(){
+            Member member = new Member();
+            member.setName("userB");
+            member.setAddress(new Address("진주","2","2222"));
+            em.persist(member);
+
+            Book book1 = new Book();
+            book1.setName("SPRING1 BOOK");
+            book1.setPrice(20000);
+            book1.setStockQuantity(200);
+            em.persist(book1);
+
+            Book book2 = new Book();
+            book2.setName("SPRING2 BOOK");
+            book2.setPrice(40000);
+            book2.setStockQuantity(300);
+            em.persist(book2);
+
+            OrderItem orderItem1 = OrderItem.creteOrderItem(book1, 20000,3);
+            OrderItem orderItem2 = OrderItem.creteOrderItem(book2, 40000,4);
 
             Delivery delivery = new Delivery();
             delivery.setAddress(member.getAddress());
