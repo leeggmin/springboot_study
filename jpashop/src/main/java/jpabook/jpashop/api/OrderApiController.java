67400ba1d0331716d5e0551774213db70b12a243
이지민 <wlals060330@gmail.com>
2022-07-19 22:23:00 +0900

    api

---
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,68 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+//            foreach
+            for (OrderItem orderItem : orderItems) {
+                orderItem.getItem().getName();
+            }
+//            lambda
+//            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2(){
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    @Data
+    static class OrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime localDateTime;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItem> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            localDateTime = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            orderItems = order.getOrderItems();
+        }
+    }
+}
