552dd8f815f2ccaf2d3afa5b3d11fc8509b08013
이지민 <wlals060330@gmail.com>
2022-07-24 23:38:43 +0900

    api

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -9,6 +9,7 @@ import jpabook.jpashop.repository.OrderSearch;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -57,8 +58,10 @@ public class OrderApiController {
     }
 
     @GetMapping("api/v3.1/orders")
-    public  List<OrderDto> ordersV3_page(){
-        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+    public  List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset",defaultValue = "0") int offset,
+            @RequestParam(value = "limit",defaultValue = "100") int limit){
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset,limit);
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
