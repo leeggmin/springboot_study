2ef6791dd84e68637dca0d3ccc2ce57a9e8944fb
이지민 <wlals060330@gmail.com>
2022-07-29 02:21:02 +0900

    user_api

--- test/src/main/java/com/example/test/api/UserJoinApiController.java
+++ test/src/main/java/com/example/test/api/UserJoinApiController.java
@@ -1,11 +1,11 @@
 package com.example.test.api;
 
+import com.example.test.domain.User;
 import com.example.test.service.UserService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,30 +14,33 @@ public class UserJoinApiController {
     private final UserService userService;
 
     @PostMapping("/join")
-    public JoinUserResponse user_join(@RequestBody JoinUserRequest userRequest){
-        String id = userService.join(userRequest.getId(), userRequest.getPw());
+    public JoinUserResponse userJoin(@RequestBody UserDto userRequest){
+        String id = userService.join(userRequest.getId(),userRequest.getName(), userRequest.getPw());
         return new JoinUserResponse(id);
     }
 
-    @Data
-    static class JoinUserRequest{
-
-        private String id;
-        private String pw;
-
-        public JoinUserRequest(String id, String pw) {
-            this.id = id;
-            this.pw = pw;
-        }
+    @GetMapping("/readOne/{id}")
+    public UserDto userRead(@PathVariable("id")String id){
+        User user = userService.readOne(id);
+        return new UserDto(user.getId(), user.getName(),user.getPw());
     }
 
     @Data
+    @AllArgsConstructor
+    static class UserDto{
+
+        private String id;
+        private String name;
+        private String pw;
+
+    }
+
+    @Data
+    @AllArgsConstructor
     static class JoinUserResponse{
 
         private String id;
 
-        public JoinUserResponse(String id) {
-            this.id = id;
-        }
     }
+
 }
