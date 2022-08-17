4d3c96d1490a47573ea227ac4f2fcfff276b403d
이지민 <wlals060330@gmail.com>
2022-08-18 00:12:51 +0900

    DEL

--- security/src/main/java/com/example/security/user/service/UserService.java
+++ security/src/main/java/com/example/security/user/service/UserService.java
@@ -15,7 +15,6 @@ public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUid(username)
