2297d946b042a49cd6d06d1cbac2adeca323ee10
이지민 <wlals060330@gmail.com>
2022-08-18 00:13:45 +0900

    JWT유틸 클래스 추가

---
+++ security/src/main/java/com/example/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,70 @@
+package com.example.security.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class JwtTokenProvider {
+
+    private String secretKey = "secret";
+
+    //토큰 유효시간 1시간
+    private Long tokenValidTime = 1000L * 60 * 60;
+
+    private UserDetailsService userDetailsService;
+
+    //Jwt 토큰 생성
+    public String createToken(String userPk, List<String> roles){
+        Claims claims = Jwts.claims().setSubject(userPk);
+        claims.put("roles", roles);
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + tokenValidTime))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    //Jwt 토큰에서 인증 정보 조회
+    public Authentication getAuthentication(String token){
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserPk(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    //토큰에서 회원 정보 추출
+    private String getUserPk(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    //Request Header 에서 token 값 가져오기 "X-AUTH-TOKEN" : "TOKEN 값"
+    public String resolveToken(HttpServletRequest request){
+        String token = null;
+        Cookie cookie = WebUtils.getCookie(request, "X-AUTH-TOKEN");
+        if (cookie != null) token = cookie.getValue();
+        return token;
+    }
+
+    //토큰의 유효셩 + 만료일자 확인
+    public boolean validateToken(String jwtToken){
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
