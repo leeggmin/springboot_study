de8b7117d1a35738db18c8afeb7e68e62186a066
이지민 <wlals060330@gmail.com>
2022-10-19 23:32:46 +0900

    chat

---
+++ chatting/src/main/java/jimin/study/chatting/domain/chat/controller/RoomController.java
@@ -0,0 +1,27 @@
+package jimin.study.chatting.domain.chat.controller;
+
+import jimin.study.chatting.domain.chat.dto.MessageDto;
+import jimin.study.chatting.domain.chat.dto.RoomCreateDto;
+import jimin.study.chatting.domain.chat.service.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/room")
+@RequiredArgsConstructor
+public class RoomController {
+
+    private final RoomService roomService;
+
+    @PostMapping("/create/{roomName}")
+    public RoomCreateDto createRoom(@PathVariable("roomName") String name) {
+        return roomService.createRoom(name);
+    }
+
+//    @GetMapping("/read-message/{roomId}")
+//    public List<MessageDto> readMessageOfRoom(@PathVariable("roomId")Long roomId) {
+//
+//    }
+}
